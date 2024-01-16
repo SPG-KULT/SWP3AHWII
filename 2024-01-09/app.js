ab5f22d2baa7c73f2515d05a7f19f5077efc3192
SPG-KULT <kul210328@spengergasse.at>
2024-01-16 11:28:11 +0100

    Weitergemacht, Hü notwendig

--- 2024-01-09/app.js
+++ 2024-01-09/app.js
@@ -1,18 +1,40 @@
+// So liegt der Normalwert bei Männern laut Deutscher Gesellschaft für Ernährung
+// im Intervall von 20 bis 25 kg / m², während er sich
+// bei Frauen im Intervall von 19 bis 24 kg / m² befindet.
+
 class Person {
     /* Gewicht in kg, Größe in m */
-    name;
+    #name;
     #gewicht;
-    groesse;
-    constructor(name, gewichtPar, groessePar) {
-        this.name = name;
+    #groesse;
+    #geschlecht;
+    constructor(namePar, gewichtPar, groessePar, geschlechtPar) {
+        this.name = namePar;
         this.gewicht = gewichtPar;
         this.groesse = groessePar;
+        this.geschlecht = geschlechtPar;
+    }
+    set geschlecht(geschlechtPar) {
+        if (typeof geschlechtPar !== 'string') {
+            throw new Error('Parameter kein String');
+        }
+        if (geschlechtPar != 'w' && geschlechtPar != 'm') {
+            throw new Error('muss m oder w sein');
+        } 
+        this.#geschlecht = geschlechtPar;
+
     }
 
-    
-    get bmi() {
-       
+    set name(namePar) {
+        if (typeof namePar !== 'string') {
+            throw new Error('ungültiger Name');
+        }
+        if (namePar.length < 3) {
+            throw new Error('Name zu kurz');
+        }
+        this.#name = namePar;
     }
+
     set gewicht(gewichtPar) {
         // gewicht in kg
         if (gewichtPar < 10 || gewichtPar > 500) {
@@ -24,28 +46,61 @@ class Person {
         return this.#gewicht;
     }
     set groesse(groessePar) {
-        if (groessePar < 0.50 || groessePar > 3.00) {
+        if (groessePar < 0.5 || groessePar > 3.0) {
             throw new Error('ungültige Groesse');
         }
-        this.groesse = groessePar;
+        this.#groesse = groessePar;
     }
-    
-    
+
     get bmi() {
-        let nmbr = this.#gewicht / (this.groesse * this.groesse)
-        return Math.round(nmbr *10) / 10
+        const nmbr = this.#gewicht / (this.#groesse * this.#groesse);
+        return Math.round(nmbr * 10) / 10;
     }
-    
+    toString() {
+        return ( 
+            `Name: ${this.#name} Gewicht: ${this.#gewicht} Größe: ${this.#groesse} Geschlecht: ${this.#geschlecht} BMI: ${this.bmi} ${this.ergebnisse()}`
+        );
+    }
+    ergebnisse() {
+        if (this.#geschlecht == "w") {
+            if (this.bmi < 19) {
+                return  "Untergewicht"
+            }
+            if (this.bmi > 24) {
+                return "Übergewicht"
+            }
+            else {
+                return "Normalgewicht"
+            }
+        }
+        if (this.#geschlecht == "m") {
+            if (this.bmi < 20) {
+                return 'Untergewicht'
+            }
+            if (this.bmi > 25) {
+                return 'Übergewicht'
+            }
+            else {
+                return 'Normalgewicht'
+            }
+        }
+        
+    }
     
 }
 a = [
-    ["Peta", 90, 1.7],
-    ["Roland",70, 1.7],
-    ["Hans", 80, 1.8],
-    ["Lisa", 50, 3.5]      //Wie schaffe ich es, dass hier ein Fehler geworfen wird? 
+    ['Peta', 90, 1.7,'m'],
+    ['Lisa', 50, 3.5,'w'], 
+    ['Roland', 70, 1.7,'w'],
+    ['Hans', 80, 1.8,'w'],
 ];
 
-b = a.map(arr => new Person (...arr));
-b.forEach((p) => console.log("Name: " + p.name + " Gewicht: " + p.gewicht + " Größe: " + p.groesse + " BMI: " + p.bmi));
-
-
+b = a.map((arr) => {
+    try {
+        return new Person(...arr);
+    } catch (e) {
+        console.log(e.message);
+        return null;
+    }
+}); // jetzt ist b ein Personen-Array
+b.forEach((p) => console.log(p + ''));

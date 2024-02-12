881bda55143356e6a7d504510f65ae8e8d2565e7
Tim Kulha <kul210328@spengergasse.at>
2024-02-12 20:30:42 +0100

    hü erldigt, ein paar Fehler übrig

---
+++ 2024-01-30/app.js
@@ -0,0 +1,110 @@
+class TicketAutomat {
+    #einnahmenGesamt;
+    #eingeworfen;
+    #anzahlPersonen;
+    #ziel;
+    constructor(initialGeld) {
+        this.einnahmenGesamt = initialGeld;
+        this.#eingeworfen = 0;
+        this.#ziel = undefined;
+    }
+    einwerfen(geld) {
+        this.#eingeworfen += geld;
+    }
+    zielEinstellen(ziel) {
+        const preis = zieleUndPreise[ziel];
+        if (preis == undefined) {
+            throw new Error('Dorthin fahre ich nicht');
+        }
+        this.#ziel = ziel;
+    }
+    set einnahmenGesamt(was) {
+        this.#einnahmenGesamt = was;
+    }
+    get einnahmenGesamt() {
+        return this.#einnahmenGesamt;
+    }
+    get eingeworfen() {
+        return this.#eingeworfen;
+    }
+    anzahlEinstellen(wieviele) {
+        if (wieviele < 1 || wieviele > 10) {
+            throw new Error('falsche Anzahl Personnen');
+        }
+        this.#anzahlPersonen = wieviele;
+    }
+    ticketKaufen() {
+        try {
+            automat.einwerfen(parseFloat(einwerfenInput.value));
+            automat.zielEinstellen(zielSelect.value);
+            automat.anzahlEinstellen(parseInt(anzahlPersonenInput.value));
+            const ticket = automat.ticketKaufen();
+            einnahmenSpan.textContent = automat.einnahmenGesamt;
+            ticketAusgabeTextarea.textContent = ticket.toString();
+            updateUI(); 
+        } catch (error) {
+            console.error(error.message);
+            ticketAusgabeTextarea.textContent = `Fehler: ${error.message}`;
+        }
+    }
+}
+const zieleUndPreise = {
+    Salzbug: 30,
+    Innsbruck: 45,
+    Klagenfurt: 40,
+    Graz: 25,
+    Bregenz: 60,
+};
+class Ticket {
+    #anzahlPersonen;
+    #ziel;
+    #gegeben;
+    #summe;
+
+    constructor(anzahl, ziel, gegeben) {
+        this.#anzahlPersonen = anzahl;
+        this.#ziel = ziel;
+        this.#gegeben = gegeben;
+        this.#summe = zieleUndPreise[this.#ziel] * this.#anzahlPersonen;
+        if (this.#gegeben < this.#summe) {
+            throw new Error('nicht genug gegeben');
+        }
+    }
+    get summe() {
+        return this.#summe;
+    }
+    toString() {
+        return `===============================
+=== Fahrkarte nach ${this.#ziel} ===
+===============================
+Einzelpreis: € ${zieleUndPreise[this.#ziel]}.-
+Anzahl der Fahrgäste: ${this.#anzahlPersonen}
+===============================
+Summe: € ${this.#summe}.-
+===============================
+gegeben: € ${this.#gegeben}.-
+Restgeld: € ${this.#gegeben - this.#summe},-
+===============================`;
+    }
+}
+
+automat = new TicketAutomat(150);
+automat.anzahlEinstellen(2);
+automat.zielEinstellen('Graz');
+
+const einwerfenInput = document.getElementById('einwerfen');
+const zielSelect = document.getElementById('ziel');
+const anzahlPersonenInput = document.getElementById('anzahlPersonen');
+const fahrpreisSpan = document.getElementById('fahrpreis');
+const guthabenSpan = document.getElementById('guthaben');
+const ticketAusgabeTextarea = document.getElementById('ticketAusgabe');
+const einnahmenSpan = document.getElementById('einnahmen');
+
+function updateUI() {
+    fahrpreisSpan.textContent = zieleUndPreise[zielSelect.value] * anzahlPersonenInput.value;
+    guthabenSpan.textContent = einwerfenInput.value;
+}
+
+einwerfenInput.addEventListener('input', updateUI);
+zielSelect.addEventListener('change', updateUI);
+anzahlPersonenInput.addEventListener('input', updateUI);
